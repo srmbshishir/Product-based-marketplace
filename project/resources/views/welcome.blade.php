495856147fd0b5067ffd6e01e01a09398b845c56
Marium <mariumsana55@gmail.com>
2021-06-30 13:18:28 +0600

    abc

--- project/resources/views/welcome.blade.php
+++ project/resources/views/welcome.blade.php
@@ -9,37 +9,7 @@
     <title>APWT Marketplace</title>
 </head>
 <body>
-    <h1>APWT Marketplace</h1>
-    <a href="/login" class="btn btn-success">Login</a><a href="/register" class="btn btn-warning">Register</a> 
-    
-    <table class="table table-striped">
-		<tr>
-            <td>Product Id</td>
-            <td>Product Name</td>
-			<td>Price</td>
-			<td>Condition</td>
-            <td>Category</td>
-            <td>Discount</td>
-            <td>Quantity</td>
-            <td>description</td>
-            <td>image</td>
-            <td>Action</td>
-		</tr>
-        @foreach ($product as $key => $data)
-            <tr>
-                <td>{{$data->id}}</td>
-                <td>{{$data->name}}</td>
-                <td>{{$data->price}}</td>
-                <td>{{$data->p_condition}}</td>
-                <td>{{$data->category}}</td>
-                <td>{{$data->discount}}</td>
-                <td>{{$data->quantity}}</td>
-                <td>{{$data->description}}</td>
-                <td><img src="/upload/{{$data->image}}" alt="" width="200px" height="150px"></td>
-                <td><a href="/register">Buy</a><br><a href="/register">Add to Cart</a></td>
-            </tr>
-        @endforeach
- 	</table>
+
     
 </body>
 </html>
